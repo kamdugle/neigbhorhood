1f7d92c4b3b9a73a01cfc50af8eb4fbd2217452c
Kamry <kamdugle@gmail.com>
2016-05-17 20:46:19 -0600

    implemented subscription updates of maps

--- src/app.js
+++ src/app.js
@@ -1,12 +1,51 @@
+
 var ViewModel = function() {
 	self = this;
-	self.places = ko.observableArray(["New York", "Atlanta"]);
+	self.places = ko.observableArray();
 	self.newItem = ko.observable();
 	self.neighborhood = ko.observable("Five Points");
 	self.city = ko.observable("Denver");
 	self.state = ko.observable("Colorado");
 	self.currentNeighborhood = ko.observable("Neighborhood, City, State");
 
+	//logic for google markers
+	self.markers = {};
+	self.currentId = 0;
+	self.uniqueId = function() {
+		return self.currentId++;
+	}
+
+
+	//Subscribe places to update google map with markers
+	function updateMarkers(changes) {
+		var length = changes.length;
+		for (var i=0; i<length; i++) {
+			var alteration = changes[i];
+			if (alteration.status === "added") {
+				//logic for additions
+				var position = alteration.value.geometry.location;
+				var id = String(position.lat()) + String(position.lng());
+				var marker = new google.maps.Marker({
+					position: position,
+					map: map
+				});
+				self.markers[id] = marker;
+
+
+			} else {
+				//logic for deletions
+				var position = alteration.value.geometry.location;
+				var id = String(position.lat()) + String(position.lng());
+				var marker = self.markers[id];
+				console.log("ID:" + id + "Markers: " + self.markers);
+				marker.setMap(null);
+				delete self.markers[id];
+
+			}
+		}
+	}
+
+	self.places.subscribe(updateMarkers, null, "arrayChange");
 
 	//Methods
 	self.changeNeighborhood = function() {
@@ -31,9 +70,35 @@ var ViewModel = function() {
 		    		service = new google.maps.places.PlacesService(map);
 		    		var request = {
 		    			location: coords,
-		    			radius: '2000'
+		    			radius: '800',
+		    			type: 'cafe'
 		    		};
-		    		service.nearbySearch(request, function(data) {console.log(data);});
+
+		    		//Replaces google places to Place Array
+		    		self.places([]);
+		    		service.nearbySearch(request, function(data) {
+		    			while (self.places().length < 6) {
+		    				var length = self.places().length;
+		    				console.log(length);
+
+		    				var found = false;
+
+		    				var requestLength = data.length;
+		    				var randomPlace = data[Math.floor(Math.random()*requestLength)];
+		    				console.log(randomPlace);
+
+
+		    				for (var i=0; i<length; i++) {
+		    					if (randomPlace === self.places()[i]) {
+		    						found = true;
+		    					}
+		    				}
+
+		    				if (!found) {
+		    					self.places.push(randomPlace);
+		    				}
+		    			}
+		    			});
 
 		    	}  else {
 		    		alert("Geocode was not successful for the following reason: " + status);
